a40f77272738416d3f6ccee5ed38e8f44d8aabf7
Mouhsen Kamil <114000503+MouhsenKamil@users.noreply.github.com>
2022-09-27 18:17:33 +0530

    Removed C Workbook solutions

--- C Workbook/for_loop.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-  int i;
-  for (i = 1; i < 11; i++) {
-    printf("%d ", i);
-  }
-return 0;
-}
--- C Workbook/while_loop.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-  int i = 1;
-  while (i <= 5) {
-    printf("%d\n", i);
-    ++i;
-  }
-  return 0;
-}
