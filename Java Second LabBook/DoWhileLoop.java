a818a27f581cd704e483fd51ff83f97b4596d8b1
Mouhsen Kamil <114000503+MouhsenKamil@users.noreply.github.com>
2022-09-27 18:17:54 +0530

    Deleted Java Second LabBook solutions

--- Java Second LabBook/DoWhileLoop.java
+++
@@ -1,9 +0,0 @@
-public class DoWhileLoop {
-  public static void main(String[] args) {
-    int x = 1;
-    do {
-      System.out.println("Value of x: " + x );
-      x++;
-    } while (x < 11);
-  }
-}
--- Java Second LabBook/PyramidPatterns/PyramidExample.java
+++
@@ -1,9 +0,0 @@
-public class PyramidExample {
-  public static void main(String[] args) {
-    for (int i=1; i <= 5; i++) {
-      for (int j=1; j <= i; j++)
-        System.out.print("* ");
-      System.out.println();
-    }
-  }
-}
