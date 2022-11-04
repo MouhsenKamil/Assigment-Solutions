4998dfd557bbd7caccffcd4395de35b0ecf68673
Mouhsen Kamil <114000503+MouhsenKamil@users.noreply.github.com>
2022-11-04 21:04:02 +0530

    Added Assignment 1-Java Solutions

---
+++ Assignment 1-Java Solutions/More Difficult Questions/PrintAnimalPattern.java
@@ -0,0 +1,11 @@
+public class PrintAnimalPattern {
+  public static void main(String[] args) {
+    System.out.printf("          '__'%n");
+    System.out.printf("          (\u00a9\u00a9)%n");
+    System.out.printf("  /========\\/%n");
+    System.out.printf(" / || %%%% ||%n");
+    System.out.printf("*  ||----||%n");
+    System.out.printf("   \u00a5\u00a5    \u00a5\u00a5%n");
+    System.out.printf("   \"\"    \"\"%n");
+  }
+}
